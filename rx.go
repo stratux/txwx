b1553ada03a78ed48de32f03f96c3564a91147a0
cyoung <cyoung@gmail.com>
2018-11-21 12:05:11 -0500

    Add observation time to transmitted data.

---
+++ rx.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	uatradio "../gouatradio"
+	"./proto"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"github.com/cyoung/uatsynth"
+	"github.com/golang/protobuf/proto"
+	"hash/crc64"
+	"net"
+	"time"
+)
+
+func main() {
+	u, err := uatradio.NewUATRadio()
+	if err != nil {
+		panic(err)
+	}
+
+	BROADCAST_IPv4 := net.IPv4(255, 255, 255, 255)
+	conn, err := net.DialUDP("udp", nil, &net.UDPAddr{
+		IP:   BROADCAST_IPv4,
+		Port: 4000,
+	})
+
+	if err != nil {
+		fmt.Printf("DialUDP(): %s\n", err.Error())
+		return
+	}
+
+	crc64Table := crc64.MakeTable(crc64.ECMA)
+
+	c := make(chan uatradio.UATRadioMessage, 1024)
+
+	u.SubscribeListener(c)
+
+	n := 0
+
+	for {
+		radioMsg := <-c
+
+		var msgLen uint16
+		var crc uint64
+
+		dataBuf := bytes.NewReader(radioMsg.Data)
+		err := binary.Read(dataBuf, binary.LittleEndian, &msgLen)
+		if err != nil {
+			fmt.Printf("binary.Read(): %s\n", err.Error())
+			continue
+		}
+		err = binary.Read(dataBuf, binary.LittleEndian, &crc)
+		if err != nil {
+			fmt.Printf("binary.Read(): %s\n", err.Error())
+			continue
+		}
+
+		if int(msgLen)+10 > len(radioMsg.Data) {
+			fmt.Printf("msgLen=%d, len(radioMsg.Data)=%d. skipping.\n", msgLen, len(radioMsg.Data))
+			continue
+		}
+
+		// Trim data according to msgLen.
+		trimmedData := radioMsg.Data[10 : msgLen+10]
+
+		// Calculate CRC of the message.
+		calculatedCRC := crc64.Checksum(trimmedData, crc64Table)
+		if crc != calculatedCRC {
+			fmt.Printf("skipping - CRC bad.\n")
+			continue
+		}
+		fmt.Printf("msgLen=%d, crc=%d, calculatedCRC=%d\n", msgLen, crc, calculatedCRC)
+
+		msg := new(txwx.WeatherMessage)
+		err = proto.Unmarshal(trimmedData, msg)
+		if err != nil {
+			fmt.Printf("err proto.Unmarshal(): %s\n", err.Error())
+			continue
+		}
+
+		fmt.Printf("OK: %s\n", msg.TextData)
+
+		// Observation time - zulu.
+		observationTime := time.Unix(int64(msg.ObservationTime), 0)
+
+		var uatMsg uatsynth.UATMsg
+		uatMsg.Decoded = true
+		uatMsg.Lat = float64(msg.StationLat)
+		uatMsg.Lon = float64(msg.StationLng)
+		uatMsg.UTCCoupled = true
+		f := new(uatsynth.UATFrame)
+		f.Text_data = []string{"METAR " + msg.TextData}
+		f.FISB_hours = uint32(observationTime.Hour())
+		f.FISB_minutes = uint32(observationTime.Minute())
+		f.Product_id = 413
+		f.Frame_type = 0
+		uatMsg.Frames = append(uatMsg.Frames, f)
+		encodedMessages, err := uatMsg.EncodeUplink()
+		if err != nil {
+			fmt.Printf("error encoding: %s\n", err.Error())
+			return
+		}
+		for _, m := range encodedMessages {
+			fmt.Printf("+")
+			for i := 0; i < len(m); i++ {
+				fmt.Printf("%02x", m[i])
+			}
+			fmt.Printf(";\n")
+			conn.Write([]byte("heyoo"))
+		}
+
+		n++
+	}
+}
--- tx.go
+++ tx.go
@@ -3,9 +3,12 @@ package main
 import (
 	uatradio "../gouatradio"
 	"./proto"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"github.com/cyoung/ADDS"
 	"github.com/golang/protobuf/proto"
+	"hash/crc64"
 	"time"
 )
 
@@ -16,6 +19,8 @@ func main() {
 		panic(err)
 	}
 
+	crc64Table := crc64.MakeTable(crc64.ECMA)
+
 	for {
 		// Get all Canadian METARs.
 		p, err := ADDS.GetADDSMETARsByIdent("C")
@@ -25,16 +30,26 @@ func main() {
 		for _, v := range p {
 			tn := time.Now().Unix()
 			msg := &txwx.WeatherMessage{
-				Type:       txwx.WeatherMessage_METAR,
-				TxTime:     uint32(tn),
-				StationLat: 44.168290,
-				StationLng: -81.629470,
-				TextData:   v.Text}
+				Type:            txwx.WeatherMessage_METAR,
+				TxTime:          uint32(tn),
+				StationLat:      44.168290,
+				StationLng:      -81.629470,
+				TextData:        v.Text,
+				ObservationTime: uint32(v.Observation.Time.Unix()),
+			}
 			fmt.Printf("%v\n", msg)
 			data, err := proto.Marshal(msg)
 			if err != nil {
 				panic(err)
 			}
+			// Add CRC.
+			crc := crc64.Checksum(data, crc64Table)
+			headerBuf := new(bytes.Buffer)
+			msgLen := uint16(len(data))
+			binary.Write(headerBuf, binary.LittleEndian, msgLen)
+			binary.Write(headerBuf, binary.LittleEndian, crc)
+			data = append(headerBuf.Bytes(), data...)
+
 			if len(data) < 150 {
 				u.TX(data)
 			}
