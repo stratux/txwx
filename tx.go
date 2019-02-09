2a1007750e8004e21d180961f9944b550b6c3978
cyoung <cyoung@gmail.com>
2019-02-08 19:08:09 -0500

    Automatically get GPS position from Stratux. Logging.

--- tx.go
+++ tx.go
@@ -1,32 +1,97 @@
 package main
 
 import (
-	uatradio "../gouatradio"
 	"./proto"
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"github.com/cyoung/ADDS"
 	"github.com/golang/protobuf/proto"
 	"github.com/kellydunn/golang-geo"
 	"hash/crc64"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 	"sync"
 	"time"
+
+	uatradio "../gouatradio"
+	humanize "github.com/dustin/go-humanize"
 )
 
 const (
-	STATION_LAT         = 44.168290
-	STATION_LNG         = -81.629470
+	SITUATION_URL       = "http://localhost/getSituation"
 	REPORTS_UPDATE_TIME = 5 * time.Minute
 )
+
+type status struct {
+	MessagesSent uint64
+}
+
+type MySituation struct {
+	GPSLatitude    float32
+	GPSLongitude   float32
+	GPSAltitudeMSL float32 // Feet MSL
+	GPSFixQuality  uint8
+	GPSTime        time.Time
+}
+
+var globalStatus status
+
+var Location MySituation
+
+var crc64Table *crc64.Table
+
+var stationGeoPt *geo.Point // Station location.
+
+var lookupMutex *sync.Mutex // Protects the following weather data variables.
+var allMETARs []ADDS.ADDSMETAR
+var allTAFs []ADDS.ADDSTAF
+
+func situationUpdater() {
+	situationUpdateTicker := time.NewTicker(1 * time.Second)
+	for {
+		<-situationUpdateTicker.C
+
+		resp, err := http.Get(SITUATION_URL)
+		if err != nil {
+			log.Printf("HTTP GET error: %s\n", err.Error())
+			continue
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("HTTP GET body error: %s\n", err.Error())
+			resp.Body.Close()
+			continue
+		}
+
+		err = json.Unmarshal(body, &Location)
+
+		if err != nil {
+			log.Printf("HTTP JSON unmarshal error: %s\n", err.Error())
+		}
+		resp.Body.Close()
+		if Location.GPSFixQuality > 0 {
+			if stationGeoPt == nil {
+				// First lock.
+				log.Printf("First GPS location obtained.\n")
+			}
+			stationGeoPt = geo.NewPoint(float64(Location.GPSLatitude), float64(Location.GPSLongitude))
+		}
+	}
+}
 
 func createMETARWeatherMessage(metar ADDS.ADDSMETAR) *txwx.WeatherMessage {
 	tn := time.Now().Unix()
 	return &txwx.WeatherMessage{
 		Type:            txwx.WeatherMessage_METAR,
 		TxTime:          uint32(tn),
-		StationLat:      STATION_LAT,
-		StationLng:      STATION_LNG,
+		StationLat:      Location.GPSLatitude,
+		StationLng:      Location.GPSLongitude,
 		TextData:        metar.Text,
 		ObservationTime: uint32(metar.Observation.Time.Unix()),
 	}
@@ -36,8 +101,8 @@ func createTAFWeatherMessage(taf ADDS.ADDSTAF) *txwx.WeatherMessage {
 	return &txwx.WeatherMessage{
 		Type:            txwx.WeatherMessage_TAF,
 		TxTime:          uint32(tn),
-		StationLat:      STATION_LAT,
-		StationLng:      STATION_LNG,
+		StationLat:      Location.GPSLatitude,
+		StationLng:      Location.GPSLongitude,
 		TextData:        taf.Text,
 		ObservationTime: uint32(taf.BulletinTime.Time.Unix()),
 	}
@@ -58,17 +123,13 @@ func preparePacketFromWeatherMessage(msg *txwx.WeatherMessage) []byte {
 	return data
 }
 
-var crc64Table *crc64.Table
-
-var stationGeoPt *geo.Point // Station location.
-
-var lookupMutex *sync.Mutex // Protects the following weather data variables.
-var allMETARs []ADDS.ADDSMETAR
-var allTAFs []ADDS.ADDSTAF
-
 func updateWeather() {
 	updateTicker := time.NewTicker(REPORTS_UPDATE_TIME)
 	for {
+		for stationGeoPt == nil {
+			log.Printf("Waiting for GPS position from Stratux...\n")
+			time.Sleep(15 * time.Second)
+		}
 		// Get all METARs within 500 sm.
 		metars, err := ADDS.GetLatestADDSMETARsInRadiusOf(500, stationGeoPt)
 		if err != nil {
@@ -88,23 +149,41 @@ func updateWeather() {
 	}
 }
 
-// Logging
-// Daemon
+func printStats() {
+	statTimer := time.NewTicker(1 * time.Minute)
+	startTime := time.Now()
+	for {
+		<-statTimer.C
+		log.Printf("stats [started: %s]\n", humanize.RelTime(startTime, time.Now(), "ago", "from now"))
+		log.Printf(" - Messages sent: %d\n", globalStatus.MessagesSent)
+	}
+}
+
 // Beacon
 
 func main() {
 	lookupMutex = &sync.Mutex{}
 
+	fp, err := os.OpenFile("/var/log/txwx.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("Unable to open logfile: %s\n", err.Error())
+		return
+	}
+	mfp := io.MultiWriter(fp, os.Stdout)
+	log.SetOutput(mfp)
+
 	u, err := uatradio.NewUATRadio()
 
 	if err != nil {
+		log.Printf("Unable to open radio: %s\n", err.Error())
 		panic(err)
 	}
 
 	crc64Table = crc64.MakeTable(crc64.ECMA)
-	stationGeoPt = geo.NewPoint(STATION_LAT, STATION_LNG)
 
-	go updateWeather()
+	go situationUpdater() // Update current station position from Stratux.
+	go updateWeather()    // Update weather data from ADDS.
+	go printStats()       // Periodically print stats.
 
 	for {
 		lookupMutex.Lock()
@@ -113,11 +192,11 @@ func main() {
 		lookupMutex.Unlock()
 
 		for _, v := range metars {
-			fmt.Printf("METAR=%s\n", v.Text)
 			msg := createMETARWeatherMessage(v)
 			data := preparePacketFromWeatherMessage(msg)
 			if len(data) < 150 {
 				u.TX(data)
+				globalStatus.MessagesSent++
 			}
 		}
 
@@ -125,11 +204,11 @@ func main() {
 			if v.Text[:4] == "TAF" {
 				v.Text = v.Text[4:]
 			}
-			fmt.Printf("TAF=%s\n", v.Text)
 			msg := createTAFWeatherMessage(v)
 			data := preparePacketFromWeatherMessage(msg)
 			if len(data) < 150 {
 				u.TX(data)
+				globalStatus.MessagesSent++
 			}
 		}
 
